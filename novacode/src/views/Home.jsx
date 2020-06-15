468860b79bd30de9995da9953ddc7fbc18c8edf1
anthonyvigz <anthonyvigz@gmail.com>
2020-06-15 19:15:44 -0400

    completed solutions module

--- novacode/src/views/Home.jsx
+++ novacode/src/views/Home.jsx
@@ -25,10 +25,66 @@ export default function Home() {
             <h3>Customize Your Package</h3>
           </div>
           <div className="solutionsBlock">
-            <div className="listedSolutions"></div>
-            <div className="additionalSolutions"></div>
+            <div className="listedSolutions">
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Website Design</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Web Applications</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Mobile Responsiveness</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Custom Business Software</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>System Design Integration</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Database Management</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>User Authentication</p>
+              </li>
+              <li className="solution">
+                <i className="fas fa-check"></i>
+                <p>Video Editing</p>
+              </li>
+            </div>
+            <div className="additionalSolutions">
+              <h4>Additional Solutions:</h4>
+              <div className="addSolution">
+                <img
+                  src="https://mk0placer5c8hgdh2ll.kinstacdn.com/wp-content/uploads/2019/12/icon1.svg"
+                  alt="Custom Reports"
+                />
+                <p>Copywriting/ Scriptwriting</p>
+              </div>
+              <div className="addSolution">
+                <img
+                  src="https://mk0placer5c8hgdh2ll.kinstacdn.com/wp-content/uploads/2019/12/icon2.svg"
+                  alt="code"
+                />
+                <p>Framework Tutorials</p>
+              </div>
+              <div className="addSolution">
+                <img
+                  src="https://mk0placer5c8hgdh2ll.kinstacdn.com/wp-content/uploads/2019/12/icon3.svg"
+                  alt="block"
+                />
+                <p>Algorithmic Services</p>
+              </div>
+            </div>
           </div>
-          <button>Quote</button>
+          <button>Learn More</button>
         </div>
       </div>
     </div>
