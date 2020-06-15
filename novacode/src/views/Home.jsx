20e0943eafb1834b337733cf9113d99426a543ff
anthonyvigz <anthonyvigz@gmail.com>
2020-06-15 18:12:59 -0400

    further home page detailing for solutions

--- novacode/src/views/Home.jsx
+++ novacode/src/views/Home.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import "../styles/home.scss";
-import Typist from "react-typist";
-import TypistLoop from "react-typist-loop";
 
 export default function Home() {
   return (
     <div className="header">
-      <h1>
-        We write code that
-        <br />
-        works for your business.
-      </h1>
+      <div class="elementor-shape elementor-shape-top" data-negative="false">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1000 100"
+          preserveAspectRatio="none"
+        >
+          <path class="elementor-shape-fill" d="M0,6V0h1000v100L0,6z"></path>
+        </svg>{" "}
+      </div>
+      <div className="homeBanner">
+        <h1>
+          We write code that
+          <br />
+          works for your business.
+        </h1>
+        <div className="whatWeDo">
+          <div className="wwdHeader">
+            <h2>Solutions</h2>
+            <h3>Customize Your Package</h3>
+          </div>
+          <div className="solutionsBlock">
+            <div className="listedSolutions"></div>
+            <div className="additionalSolutions"></div>
+          </div>
+          <button>Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
