aad275169570c4113679b88d1e46ed3826f73e2c
anthonyvigz <anthonyvigz@gmail.com>
2020-06-24 21:10:31 -0400

    created all piece objects

--- novacode/src/views/Work.jsx
+++ novacode/src/views/Work.jsx
@@ -22,14 +22,87 @@ export default function Work() {
   };
 
   const imagelinks = [
-    "https://i.imgur.com/mrWcNoF.png",
-    "https://i.imgur.com/ydYqINx.png",
-    "https://i.imgur.com/VLRCfXM.png",
-    "https://i.imgur.com/jyXGXIA.png",
-    "https://i.imgur.com/igevTeB.png",
-    "https://i.imgur.com/LxyNHq3.png",
-    "https://i.imgur.com/ZA2u5Yc.png",
-    "https://i.imgur.com/0gXCgkE.png",
+    {
+      name: "TouchBase",
+      color: "#131b37",
+      website: "https://touch-base.us",
+      github: "https://github.com/Touch-Base",
+      frameworks: ["React", "Node.js", "Redux", "Express"],
+      imagelink: "https://i.imgur.com/mrWcNoF.png",
+      description:
+        "Web and mobile app designed to take the stress out of the job application process for job-seeking professionals",
+    },
+    {
+      name: "McGarvin Engineering Website",
+      color: "#603b27",
+      website: "https://mcgarvin.com",
+      github: null,
+      frameworks: ["React", "AWS", "SASS"],
+      imagelink: "https://i.imgur.com/ydYqINx.png",
+      description:
+        "Fully functional and responsive website design for McGarvin Engineering.",
+    },
+    {
+      name: "Conway's Game of Life",
+      color: "#000000",
+      website: "PUT LINK HERE",
+      github:
+        "https://github.com/anthonyvigz/GameOfLife/tree/master/gameoflife",
+      frameworks: ["React", "Python"],
+      imagelink: "https://i.imgur.com/jyXGXIA.png",
+      description:
+        "Interpretation of Conway's Game of Life; a visual representation of an algorithmic solution.",
+    },
+    {
+      name: "Freehold",
+      color: "#6b54df",
+      website: "https://freehold.dev",
+      github: "https://github.com/Lambda-School-Labs/property-manager-fe",
+      frameworks: ["React", "Node.js", "Redux", "Express"],
+      imagelink: "https://i.imgur.com/igevTeB.png",
+      description:
+        "Web app designed to increase communication between property owners and tenants.",
+    },
+    {
+      name: "Hogger",
+      color: "#00cacd",
+      website: null,
+      github: "https://github.com/steveo-app",
+      frameworks: ["React", "Node.js", "Redux", "Express"],
+      imagelink: "https://i.imgur.com/VLRCfXM.png",
+      description:
+        "Mobile web app that keeps track of disc golf scores and courses.",
+    },
+    {
+      name: "EAF Custom Decks",
+      color: "#0b9b41",
+      website: "https://eafcustomdeck.com",
+      github: "https://github.com/anthonyvigz/EAFWebsite/tree/master/eaf",
+      frameworks: ["React", "AWS", "SASS"],
+      imagelink: "https://i.imgur.com/LxyNHq3.png",
+      description:
+        "Fully functional and responsive website design for EAF Custom Decks.",
+    },
+    {
+      name: "Blue Sky Lab",
+      color: "#0082d2",
+      website: "INSERT LINK HERE",
+      github: "https://github.com/anthonyvigz/BlueSky",
+      frameworks: ["React", "AWS", "SASS"],
+      imagelink: "https://i.imgur.com/ZA2u5Yc.png",
+      description:
+        "Fully functional and responsive website design for Blue Sky Lab.",
+    },
+    {
+      name: "Stock Market Trading Algorithm",
+      color: "#b77c6b",
+      website: null,
+      github: null,
+      frameworks: ["Python", "Polygon", "Alpaca", "WebSockets"],
+      imagelink: "https://i.imgur.com/0gXCgkE.png",
+      description:
+        "Patented and protected multi-module trading algorithm designed to make trades in the stock market on certain conditions.",
+    },
   ];
 
   return (
